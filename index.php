eb65dee36c7c6c9bda7537e7b91e51d77973d2a7
rijad.dizdarevic <rijad.dizdarevic@gmail.com>
2017-02-02 10:13:14 -0700

    post toppings

--- index.php
+++ index.php
@@ -68,7 +68,26 @@ $app->get('/toppings', function () use ($app){
 
 $app->post('/toppings', function () use ($app){
 	 $body_params = json_decode($app->request->getBody());
-	 echo "toppings post";
+	 if ($body_params->topping !== NULL && $body_params->topping->name !==null) {
+	 	global $mysqli;
+	 	
+	 	 $result = $mysqli->query("SELECT * FROM topping WHERE name = '" . $body_params->topping->name . "'");
+
+ 		 if ($result->num_rows > 0) {
+ 		 	 echo "Topping already exists.";
+ 		 } else {
+ 		 	$sql = "INSERT INTO topping (name) VALUES ('". $body_params->topping->name ."')";
+
+			 if ($mysqli->query($sql) === TRUE) {
+				 echo "New record created successfully";
+			 } else {
+				 echo "Error: " . $sql . "<br>" . $mysqli->error;
+			 }
+ 		 }
+	 	$mysqli->close();
+	 } else {
+	 	echo "no topping to add";
+	 }
 });
 
 $app->get('/pizzas/:pizza_id/toppings', function ($pizza_id) {
